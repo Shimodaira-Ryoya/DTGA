3fd32781cb897b46b434e48544326bb2eed81362
Shimodaira-ryoya <24w6036e@shinshu-u.ac.jp>
2025-06-03 16:29:06 +0900

    svm

--- source/SVM/svm.py
+++ source/SVM/svm.py
@@ -7,12 +7,13 @@ from sklearn.metrics import accuracy_score,f1_score
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
 """データセットの準備※随時調整"""
-df=pd.read_csv('../../database/drybeans_dataset.csv')
+df=pd.read_csv('../../database/pima_dataset.csv')
 X_df=df.iloc[:,1:-1]
 y_df=df.iloc[:,-1]
 X =X_df.values
 xname=X_df.columns.tolist()
 y,yname= pd.factorize(y_df)#値を整数値にエンコード
+y,yname=y_df.values,[0,1]
 print('table_shape;',X_df.shape)
 print('class sample;',y_df.value_counts())
 """データのk分割"""
@@ -21,23 +22,31 @@ kf=StratifiedKFold(n_splits=10,shuffle=True,random_state=0)
 for fold, (train_idx, test_idx) in enumerate(kf.split(X,y)):
     print("train;",Counter(y[train_idx]))
     print("test;",Counter(y[test_idx]))
+print(df.columns)
+"""#%%
+#データ分布確認
+import seaborn as sns
+import matplotlib.pyplot as plt
+for x in X_df.columns:
+    sns.histplot(data=df,x=x,hue='Outcome')
+    plt.show()"""
     
 #%%
 """Parameter Set"""
-outputfolder='../../output/simple_svm/adult/rbf1'
+outputfolder='../../output/simple_svm/pima/rbf_default'
 f1_score_average='binary'#f値の計測タイプ#binary(バイナリ),macro(均衡なデータ向き),weighted(不均衡なデータ向き),micro(全体の精度的なスコア)
 kernel='rbf' # カーネル: 'linear', 'rbf', 'poly', 'sigmoid' など
-c=1#[0.1, 1, 10, 100],
-
-
 # %%
 """Folder Creation"""
 os.mkdir(outputfolder)
 """Model Training & Evaluate"""
 datas=[]
-for i, (train_idx, test_idx) in enumerate(kf.split(X_scaled,y)):
-    """Train"""
+for i, (train_idx, test_idx) in enumerate(kf.split(X,y)):
+    """Scaling and Training"""
     Xtr,Xte=X[train_idx],X[test_idx]#データをindexに応じて抽出
+    scaler = StandardScaler()#データを正規化（特徴量スケーリング）
+    Xtr_scaled = scaler.fit_transform(Xtr)#トレーニングデータに対して正規化を行っている？※分割前に正規化はいけない。テストデータに合わさって正規化されることになりカンニングになる
+    Xte_scaled = scaler.transform(Xte)#トレーニングデータのパラメータでテストデータを正規化している？
     ytr,yte=y[train_idx],y[test_idx]
     svm = SVC(kernel=kernel,class_weight='balanced') 
     svm.fit(Xtr, ytr)
